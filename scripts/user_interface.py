203a73e31a74a533ec230b3bd7404bd12afed971
Danzi Federico <etruria89@hotmail.it>
2021-05-22 06:39:36 +0000

    UI restored

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -3,11 +3,6 @@ import time
 from rt2_assignment1.srv import Command
 
 def main():
-    """
-    Definition of the command line
-    user interface to set the "start"
-    "stop" varaible.
-    """
     rospy.init_node('user_interface')
     ui_client = rospy.ServiceProxy('/user_interface', Command)
     time.sleep(10)
@@ -18,7 +13,7 @@ def main():
             ui_client("start")
             x = int(input("\nPress 0 to stop the robot "))
         else:
-            print("The robot stopped.")
+            print("Please wait, the robot is going to stop when the position will be reached")
             ui_client("stop")
             x = int(input("\nPress 1 to start the robot "))
             
