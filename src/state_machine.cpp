28a9bd0b22bfe56ecc5b1a851fdf65bca093e486
Danzi Federico <etruria89@hotmail.it>
2021-05-23 20:56:17 +0100

    FSM updated

--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -17,6 +17,17 @@ int state = 0;
 */
 
 
+	  /****************************************//**
+	  * Service callback setting the start/stop
+	  * robot state
+	  *
+	  *   Service call header (unused).
+	  * \param req (const std::shared_ptr<Command::Request>):
+	  *   Service request, containing the command (string).
+	  * \param res (const std::shared_ptr<Command::Response>):
+	  *   Service response, the value of the 'start' state (bool).
+	  *
+	  ********************************************/ 
 
 bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
     if (req.command == "start"){
@@ -28,18 +39,33 @@ bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Com
     return true;
 }
 
-void doneCllbck(const actionlib::SimpleClientGoalState& goal_state,
+/****************************************//**
+* Callback launched at the end of the action
+*
+* This function is launched once the goal of
+* of the action is reached.
+*
+* \param goal_state (const actionlib::SimpleClientGoalState&):
+*   The goal state reached.
+* \param result (const rt2_assignment1::PoseResultConstPtr&):
+*   The result of the action.
+*
+********************************************/ 
+
+void done(const actionlib::SimpleClientGoalState& goal_state,
                 const rt2_assignment1::Control2_1ResultConstPtr& result){    
     state = 2;
 }
-void activeCllbck(){return;}
+
+/*void activeCllbck(){return;}
 
 void feedbackCllbck(const rt2_assignment1::Control2_1FeedbackConstPtr& feedback){
     ROS_INFO("FEEDBACK: %s", feedback->status.c_str());
-}
+}*/
 
 int main(int argc, char **argv)
 {
+   /* Action client definition */	
    ros::init(argc, argv, "state_machine");
    ros::NodeHandle n;
    ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
@@ -53,7 +79,7 @@ int main(int argc, char **argv)
    rp.request.y_min = -5.0;
    rt2_assignment1::Control2_1Goal goal;
      //wait for the action server to come up
-    while(!ac.waitForServer(ros::Duration(5.0))){ // timeout can be set also for the waiforserrver
+    while(!ac.waitForServer(ros::Duration(10.0))){ // timeout can be set also for the waiforserrver
         ROS_INFO("Waiting for the go_to_point action server to come up");
     }
    
@@ -61,15 +87,19 @@ int main(int argc, char **argv)
        	ros::spinOnce();
        	switch (state){
            	case 1:
+		/*  Go to the goal  */
                 client_rp.call(rp);
+			/*  Goal set  */
                 goal.x = rp.response.x;
             	goal.y = rp.response.y;
                	goal.theta = rp.response.theta;
                	std::cout << "\nGoing to the position: x= " << goal.x << " y= " << goal.y << " theta = " << goal.theta << std::endl;
-               	ac.sendGoal(goal, &doneCllbck, &activeCllbck, &feedbackCllbck);
+			/*  Send goal information and monitor the   */
+               	ac.sendGoal(goal, &done)/*, &activeCllbck, &feedbackCllbck);*/
                	state = 0;
                	break;
             case -1:
+		/*  Stop the action cancelling the Goal  */
                 ac.cancelGoal();
                 state = 0;
                 break;
@@ -78,15 +108,13 @@ int main(int argc, char **argv)
                 
 	            actionlib::SimpleClientGoalState goal_state = ac.getState();
                 if(goal_state == actionlib::SimpleClientGoalState::SUCCEEDED){
+			/*  goal reached state */
                     ROS_INFO("Pose goal reached");
                     state = 1;
                 }
                 else if(goal_state == actionlib::SimpleClientGoalState::PREEMPTED){
+			/*  goal cancelled state */
                     ROS_INFO("Goal canceled");
-                    state = 0;
-                }
-                else{
-                    ROS_INFO("Failed to reach the goal pose");
                     state = 0;
                 }
                 
