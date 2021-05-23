307c1c78433599fdef863a5c5b99b2942324d8d4
etruria89 <etruria89@hotmail.it>
2021-05-23 23:30:50 +0000

    docs folder updated

---
+++ docs/python/html/search/functions_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['fix_5ffinal_5fyaw_32',['fix_final_yaw',['../namespacego__to__point.html#a4a406386ca62830c19c93949e923f7bd',1,'go_to_point']]],
+  ['fix_5fyaw_33',['fix_yaw',['../namespacego__to__point.html#aa2f5974f6134483d69194c980a3a0799',1,'go_to_point']]]
+];
